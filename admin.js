dd00a859b1b59b8d472e87517d96b0609a4bf293
GitHubJustForIt <151542280+GitHubJustForIt@users.noreply.github.com>
2026-07-10 16:37:16 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const SHEETS_API =
-  "https://script.google.com/macros/s/AKfycbzxaTffjMadwWagMiTxxVxyc6QkLDN8AuYP2yiz75CyguhnXGWHII4iR7uABPa2eD6I/exec";
+  "https://script.google.com/macros/s/AKfycbxZ0DJ07Zdh2B1mC5K2a7jFxcttaTva8_Lwkiril-7rz0lXZpCusSDwFnlamSP_mVbC/exec";
 
 const ADMIN_CODE = "19.08.2011";
 const ADMIN_SESSION_KEY = "plr_admin_ok";
